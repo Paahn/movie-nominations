74799b25bbb3d812c516f4e854f3b3a9deebec11
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-17 13:52:31 -0800

    added style to nomination component

--- src/components/AddNomination.js
+++ src/components/AddNomination.js
@@ -1,10 +1,11 @@
 import React from "react";
 import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
+import "../styles/AddNomination.css";
 
 const AddNomination = () => {
   return (
     <div>
-      <span><LibraryAddIcon />Nominate</span>
+      <span className="nomination"><LibraryAddIcon />Nominate</span>
     </div>
   )
 }
