9c94ae375e83c122301a88f3089246201d580b67
Panos Hatzinikolaou <pjhatzis@gmail.com>
2021-01-21 14:27:08 -0800

    added logic to activate nominate button when there are less than 5 noms and deactivate when there are 5 noms

--- src/components/AddNomination.js
+++ src/components/AddNomination.js
@@ -2,10 +2,15 @@ import React from "react";
 import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
 import "../styles/AddNomination.css";
 
-const AddNomination = () => {
+const AddNomination = ({ active, inactive }) => {
+  console.log(`Active? ${active} Inactive? ${inactive}`);
   return (
     <div className="nomination-container">
-      <span className="nomination"><EmojiEventsIcon color="primary"/>Nominate</span>
+      <span 
+        className={`nomination ${active === 'true' ? 'nomination-available' : ''} ${inactive === 'true' ? 'nomination-unavailable' : ''}`}>
+          <EmojiEventsIcon color="primary"/>
+        Nominate
+      </span>
     </div>
   )
 }
